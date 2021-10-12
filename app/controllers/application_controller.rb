6944a5c050c9ca90cee98969cde551a119980b04
Kole565 <rjkz565rjkz565@gmail.com>
2021-10-12 21:17:53 +0300

    Exercuse 1.7.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
 		render plain: "hello, world!" # Plain because simple text
 	end
 
+	def hello_special
+		render plain: "¡Hola, Mundo!" # Exercuse 1.7 RoR book
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-	root "application#hello" # Link to hello method in aplication_controller
+	# root "application#hello" # Link to hello method in aplication_controller
+	root "application#hello_special" # For exercuse 1.7
 
 end
